a565b3741f999ea94b2513a18c6dbf0c1dd00886
kingdiat <kingdiat@smail.cyc.edu.tw>
2022-02-04 21:50:02 +0800

    index.js added

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("Hello, webpack")
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+app.get('/', function(req, res) {
+    if (JSON.stringify(req.query) === '{}') {
+        res.sendFile(__dirname + '/index0.html');
+    }
+    else {
+        res.sendFile(__dirname + '/' + Object.keys(req.query)[0]);
+    }
+
+});
+
+app.listen(80, function() {
+    console.log('Express started!');
+});
 
